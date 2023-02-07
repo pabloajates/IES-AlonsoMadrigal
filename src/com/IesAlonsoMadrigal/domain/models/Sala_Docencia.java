18381e2ca67f3e4ab2c5eb0a24fc9276a3475fba
Pablo Ajates <pablo.ajasan@educa.jcyl.es>
2023-02-07 12:38:52 +0100

    Creadas las distintas clases

--- src/com/IesAlonsoMadrigal/domain/models/Sala_Docencia.java
+++ src/com/IesAlonsoMadrigal/domain/models/Sala_Docencia.java
@@ -1,4 +1,49 @@
 package com.IesAlonsoMadrigal.domain.models;
 
 public class Sala_Docencia {
+    private Integer id;
+    private String planta;
+    private String sillas;
+    private String capacidad;
+    private String tipo_pizarra;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(String planta) {
+        this.planta = planta;
+    }
+
+    public String getSillas() {
+        return sillas;
+    }
+
+    public void setSillas(String sillas) {
+        this.sillas = sillas;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getTipo_pizarra() {
+        return tipo_pizarra;
+    }
+
+    public void setTipo_pizarra(String tipo_pizarra) {
+        this.tipo_pizarra = tipo_pizarra;
+    }
 }
--- src/com/IesAlonsoMadrigal/domain/models/Sala_Ordenadores.java
+++ src/com/IesAlonsoMadrigal/domain/models/Sala_Ordenadores.java
@@ -1,4 +1,50 @@
 package com.IesAlonsoMadrigal.domain.models;
 
 public class Sala_Ordenadores {
+
+    private Integer id;
+    private String planta;
+    private String num_ordenadores;
+    private String capacidad;
+    private String tipo_ordenadores;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(String planta) {
+        this.planta = planta;
+    }
+
+    public String getNum_ordenadores() {
+        return num_ordenadores;
+    }
+
+    public void setNum_ordenadores(String num_ordenadores) {
+        this.num_ordenadores = num_ordenadores;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getTipo_ordenadores() {
+        return tipo_ordenadores;
+    }
+
+    public void setTipo_ordenadores(String tipo_ordenadores) {
+        this.tipo_ordenadores = tipo_ordenadores;
+    }
 }
