7471304f17b9bbfcd40f52363a494dc0bc91105b
enriiqueee <kikiarenas46@gmail.com>
2023-02-08 12:58:43 +0100

    Agregado en matriculas alumnos

--- src/com/IesAlonsoMadrigal/domain/models/Matriculas.java
+++ src/com/IesAlonsoMadrigal/domain/models/Matriculas.java
@@ -4,6 +4,7 @@ public class Matriculas {
 
     private Integer id;
     private Curso curso;
+    private Alumnos alumnos;
 
     public Integer getId() {
         return id;
@@ -19,5 +20,13 @@ public class Matriculas {
 
     public void setCurso(Curso curso) {
         this.curso = curso;
+    }
+
+    public Alumnos getAlumnos(){
+        return alumnos;
+    }
+
+    public void setAlumnos(Alumnos alumnos){
+        this.alumnos=alumnos;
     }
 }
